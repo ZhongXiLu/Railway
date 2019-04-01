37b3d760b9c5f2e70c629a835abed8e2c692e851
ZhongXiLu <zhongxilu@hotmail.com>
2019-04-01 20:02:16 +0200

    Unity: added interface for creating tracks objects

--- Unity/Railway/Assets/Scripts/CameraMovement.cs
+++ Unity/Railway/Assets/Scripts/CameraMovement.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
+/**
+Basic script to move around the camera.
+*/
+public class CameraMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // TODO
+    void Update() {
+        transform.Translate(speed * new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")));
     }
 }
